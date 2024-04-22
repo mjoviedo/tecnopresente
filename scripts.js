c5173ece3bd766abd3fe520776a04e9cfa290a59
mjoviedo <128503807+mjoviedo@users.noreply.github.com>
2024-04-22 12:17:10 -0300

    arreglos footer

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();
+  });
+
